142cd35d32aa1381669b1aab42569190fcc5063f
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-10 18:59:16 +0300

    add

--- Controller/Controller.java
+++ Controller/Controller.java
@@ -1,7 +1,14 @@
 package com.MAX.Controller;
 
+import com.MAX.Services.RequestDTO;
 import com.MAX.Services.Service;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping("/sigma")
 public class Controller {
 
     private Service service;
@@ -9,5 +16,14 @@ public class Controller {
     public Controller(Service service) {
         this.service = service;
     }
+
+    @PostMapping(value = "/a",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseDTO add(@RequestBody RequestDTO user){
+        return service.create(user);
+
+    }
+
+    public
 
 }
