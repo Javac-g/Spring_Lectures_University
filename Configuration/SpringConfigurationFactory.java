8a1d9ec6bff136b2ee19b033e911b52bfd5a4fb3
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-10 18:19:34 +0300

    Configuration

---
+++ Configuration/SpringConfigurationFactory.java
@@ -0,0 +1,26 @@
+package com.MAX.Configuration;
+
+import com.MAX.Controller.Controller;
+import com.MAX.Services.Service;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class SpringConfigurationFactory {
+    @Bean(name = "Service")
+    public Service serviceBean(){
+
+        return new Service();
+
+    }
+    @Bean(name = "Controller")
+    @Scope(value = "prototype")
+    public Controller controllerBean(){
+
+        return new Controller(serviceBean());
+
+    }
+
+
+}
--- Controller/Controller.java
+++ Controller/Controller.java
@@ -1,4 +1,13 @@
 package com.MAX.Controller;
 
+import com.MAX.Services.Service;
+
 public class Controller {
+
+    private Service service;
+
+    public Controller(Service service) {
+        this.service = service;
+    }
+
 }
