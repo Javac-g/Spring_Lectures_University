45bbecf90825fe6e614ac5fa31b1d56423ebf609
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-10 18:11:43 +0300

    Response

--- Controller/ResponseDTO.java
+++ Controller/ResponseDTO.java
@@ -1,4 +1,42 @@
 package com.MAX.Controller;
 
+import com.MAX.Services.CarEnum;
+
 public class ResponseDTO {
+    String name;
+    Integer id;
+    Tool tool;
+    CarEnum carEnum;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Tool getTool() {
+        return tool;
+    }
+
+    public void setTool(Tool tool) {
+        this.tool = tool;
+    }
+
+    public CarEnum getCarEnum() {
+        return carEnum;
+    }
+
+    public void setCarEnum(CarEnum carEnum) {
+        this.carEnum = carEnum;
+    }
 }
