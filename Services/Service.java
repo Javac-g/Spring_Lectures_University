434e5ec4b7fabad09a1d4f04703e0f17ee27c22b
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-10 18:28:16 +0300

    log

--- Services/Service.java
+++ Services/Service.java
@@ -2,6 +2,10 @@ package com.MAX.Services;
 
 import com.MAX.Controller.ResponseDTO;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,5 +17,21 @@ public class Service {
         return msg;
 
     }
+    public  void log(String type, ResponseDTO user){
+        byte[] data = ("\nName: " + user.getName()).getBytes();
+        try(ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            FileOutputStream fileOutputStream = new FileOutputStream(type,true);
+            DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream)){
 
+
+            byteArrayOutputStream.write(data);
+            byteArrayOutputStream.writeTo(fileOutputStream);
+            dataOutputStream.writeUTF("\nID: " + user.getId());
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+
+        }
+    }
 }
