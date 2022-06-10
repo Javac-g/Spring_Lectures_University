e8b75b008fb921ecdf91f3e07e42ba285b9a69c7
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-10 18:22:27 +0300

    print

--- Services/Service.java
+++ Services/Service.java
@@ -1,4 +1,17 @@
 package com.MAX.Services;
 
+import com.MAX.Controller.ResponseDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Service {
+    List<ResponseDTO> database = new ArrayList<>();
+
+    public String print(String msg){
+
+        return msg;
+
+    }
+
 }
