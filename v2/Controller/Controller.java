ff9233e21a39f72e37572a6ba948877de2bef62c
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-14 19:46:28 +0300

    add

--- v2/Controller/Controller.java
+++ v2/Controller/Controller.java
@@ -1,7 +1,12 @@
 package com.MAX.v2.Controller;
 
+import com.MAX.v2.Service.RequestDTO;
 import com.MAX.v2.Service.Service;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping("/omega")
 public class Controller {
@@ -11,6 +16,14 @@ public class Controller {
     public Controller(Service service){
 
         this.service = service;
+
+    }
+
+    @PostMapping(value = "/a",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseDTO add(@RequestBody RequestDTO json){
+
+        return service.create(json);
 
     }
 
