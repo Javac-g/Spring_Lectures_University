39eff43f583059c1b530327786cafb3721cb5e46
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-14 18:51:41 +0300

    зкÑprint

--- v2/Service/Service.java
+++ v2/Service/Service.java
@@ -1,4 +1,16 @@
 package com.MAX.v2.Service;
 
+import com.MAX.v2.Controller.ResponseDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Service {
+    List<ResponseDTO> datalist = new ArrayList<>();
+
+    public String print(Integer id){
+
+        return"Person № - [" + id + "] was deleted ";
+
+    }
 }
