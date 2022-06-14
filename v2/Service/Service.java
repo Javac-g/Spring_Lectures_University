4ac0b38333b402926fef565286ddfe2b8ffba610
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-14 19:09:55 +0300

    зlog

--- v2/Service/Service.java
+++ v2/Service/Service.java
@@ -2,6 +2,10 @@ package com.MAX.v2.Service;
 
 import com.MAX.v2.Controller.ResponseDTO;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,5 +16,22 @@ public class Service {
 
         return"Person № - [" + id + "] was deleted ";
 
+    }
+    public void log(String type,ResponseDTO user){
+        byte[] data = ("\nType: " + type+"\nName: " + user.getName()).getBytes();
+        try(ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            FileOutputStream fileOutputStream = new FileOutputStream("log.dat");
+            DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream)){
+
+            byteArrayOutputStream.write(data);
+            byteArrayOutputStream.writeTo(fileOutputStream);
+            dataOutputStream.writeUTF("\nID: " + user.getId());
+
+
+        }catch(IOException e){
+
+            e.printStackTrace();
+
+        }
     }
 }
