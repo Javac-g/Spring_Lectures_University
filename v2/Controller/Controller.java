759ffb0c27a4b238fe8e2e59f8d2da6feac64f2f
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-14 19:50:14 +0300

    find

--- v2/Controller/Controller.java
+++ v2/Controller/Controller.java
@@ -3,10 +3,7 @@ package com.MAX.v2.Controller;
 import com.MAX.v2.Service.RequestDTO;
 import com.MAX.v2.Service.Service;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/omega")
 public class Controller {
@@ -25,6 +22,12 @@ public class Controller {
 
         return service.create(json);
 
+    }
+    @GetMapping(value = "/f",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseDTO find(@RequestParam Integer id){
+
+        return service.read(id);
     }
 
 
