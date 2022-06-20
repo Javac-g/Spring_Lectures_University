f2c379ea2d8b597eeb1aea5428b6749c4ed59667
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-20 23:06:13 +0300

    add

--- v3/Controller/Controller.java
+++ v3/Controller/Controller.java
@@ -1,12 +1,27 @@
 package com.MAX.v3.Controller;
 
+import com.MAX.v3.Service.ResponseDTO;
 import com.MAX.v3.Service.Service;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@RequestMapping("/App")
 public class Controller {
 
     private Service service;
 
     public Controller(Service service) {
         this.service = service;
+    }
+
+    @PostMapping(value = "/a",produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public ResponseDTO create(@RequestBody RequestDTO json){
+
+        return service.create(json);
+
     }
 }
