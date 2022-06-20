5ace4fd71efdc09337283267ec8b00bc1de741bc
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-20 22:10:02 +0300

    tool

---
+++ v3/Controller/Tool.java
@@ -0,0 +1,22 @@
+package com.MAX.v3.Controller;
+
+public class Tool {
+
+    String first,second;
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getSecond() {
+        return second;
+    }
+
+    public void setSecond(String second) {
+        this.second = second;
+    }
+}
