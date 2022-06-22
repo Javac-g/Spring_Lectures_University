8cc74550cefa1b60637bc1d6cd3a45e906700929
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-22 21:20:07 +0300

    controller

---
+++ v4/Controller/Controller.java
@@ -0,0 +1,9 @@
+package com.MAX.v4.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vk")
+public class Controller {
+}
