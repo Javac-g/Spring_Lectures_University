a74873687a3bbc6d3f3dcc8b3e658f5675d51487
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-25 22:52:57 +0300

    log

--- v5/Services/Service.java
+++ v5/Services/Service.java
@@ -1,5 +1,9 @@
 package com.MAX.v5.Services;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,5 +12,26 @@ public class Service {
 
     public String print(String msg){
         return msg;
+    }
+
+    public void log(String type,ResponseDTO user){
+        byte[] data = ("\nType: " + type + "\nName: " + user.getName()).getBytes();
+        String str = "ID: " + user.getId();
+
+        try(FileOutputStream fileOutputStream = new FileOutputStream("log.dat",true);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream)){
+
+            byteArrayOutputStream.write(data);
+            byteArrayOutputStream.writeTo(fileOutputStream);
+            dataOutputStream.writeUTF(str);
+
+        }catch (IOException e){
+
+            e.printStackTrace();
+
+        }
+
+
     }
 }
