77f3e2ce36c3218e965fc43306ace56d6bc9043c
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-26 21:06:58 +0300

    request

---
+++ v6/Controller/RequestDTO.java
@@ -0,0 +1,43 @@
+package com.MAX.v6.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RequestDTO {
+    String name;
+    Integer id;
+    Tool tool;
+    List<Car> carlist = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Tool getTool() {
+        return tool;
+    }
+
+    public void setTool(Tool tool) {
+        this.tool = tool;
+    }
+
+    public List<Car> getCarlist() {
+        return carlist;
+    }
+
+    public void setCarlist(List<Car> carlist) {
+        this.carlist = carlist;
+    }
+}
