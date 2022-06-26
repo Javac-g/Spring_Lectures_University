ac87b73213fcf2605d2b474ef6aab69059638e1b
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-06-26 23:57:49 +0300

    Mapping controller

--- v6/Controller/Controller.java
+++ v6/Controller/Controller.java
@@ -1,4 +1,15 @@
 package com.MAX.v6.Controller;
 
+import com.MAX.v6.Services.ServiceMain;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/vj")
 public class Controller {
+
+    private ServiceMain main;
+
+    public Controller(ServiceMain main) {
+        this.main = main;
+    }
+
 }
