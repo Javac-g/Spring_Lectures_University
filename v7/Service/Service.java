5488021a40bb1a0ed38def1c6b48528ac2208272
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-07-06 14:01:47 +0300

    service

--- v7/Service/Service.java
+++ v7/Service/Service.java
@@ -1,4 +1,12 @@
 package com.MAX.v7.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Service {
+    List<ResponseDTO> datalist = new ArrayList<>();
+
+    public String print(String msg){
+        return msg;
+    }
 }
