97ecdc94f7b0dde608ab65cd8c45357497a35e36
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-07-06 14:20:44 +0300

    log

--- v7/Service/Service.java
+++ v7/Service/Service.java
@@ -1,5 +1,10 @@
 package com.MAX.v7.Service;
 
+import com.sun.xml.internal.messaging.saaj.util.ByteOutputStream;
+
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,5 +13,21 @@ public class Service {
 
     public String print(String msg){
         return msg;
+    }
+    public void log(String type, ResponseDTO user){
+        byte[] data = ("\nType: " + type + "\nName: " + user.getName() + "\nId: " + user.getId()).getBytes();
+        String additional = "\nTool: first - " + user.getTool().getFirst()+"\nTool: - second"+ user.getTool().getSecond() +"\nCar: "+ user.getCarEnum();
+
+        try(FileOutputStream fileOutputStream = new FileOutputStream("logger.dat",true);
+            ByteOutputStream byteOutputStream = new ByteOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream)){
+
+            byteOutputStream.write(data);
+            byteOutputStream.writeTo(fileOutputStream);
+            dataOutputStream.writeUTF(additional);
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
