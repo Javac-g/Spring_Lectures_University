e07affe916442b95e8828daa23a287b27b78bc21
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-07-06 13:18:03 +0300

    Car

---
+++ v7/Controller/Car.java
@@ -0,0 +1,13 @@
+package com.MAX.v7.Controller;
+
+public class Car {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
