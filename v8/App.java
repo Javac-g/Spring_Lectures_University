39b61ba3d4c404cb25affdbe9ab5185df7a232cb
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-07-09 20:38:20 +0300

    App

---
+++ v8/App.java
@@ -0,0 +1,12 @@
+package com.MAX.v8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String...args){
+
+        SpringApplication.run(App.class,args);
+    }
+}
