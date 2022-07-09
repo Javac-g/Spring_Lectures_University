94cf2556bdf135af28559916f93fd8a34ab1a0a6
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-07-09 21:03:07 +0300

    print

---
+++ v8/Service/Service_Main.java
@@ -0,0 +1,14 @@
+package com.MAX.v8.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Service_Main {
+
+    private List<ResponseDTO> datalist = new ArrayList<>();
+
+    public String print(String msg){
+        return msg;
+    }
+
+}
