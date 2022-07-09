c3aef9aa03c762bfe2b08054b140145de1270470
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-07-09 21:12:10 +0300

    logger

--- v8/Service/Service_Main.java
+++ v8/Service/Service_Main.java
@@ -1,5 +1,9 @@
 package com.MAX.v8.Service;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,5 +14,28 @@ public class Service_Main {
     public String print(String msg){
         return msg;
     }
+    public void log(String type,ResponseDTO user){
 
+        byte[] data = ("\nType: " + type + "\nName: " + user.getName()+ "\nId: " + user.getId()).getBytes();
+        String A = "\nTool - A: "+ user.getTool().getFirst() + "\nTool - A: " + user.getTool().getSecond();
+        String B = "\nCar: " + user.getCarEnum();
+
+        try(FileOutputStream fileOutputStream = new FileOutputStream("Logger.dat",true);
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            DataOutputStream dataOutputStream = new DataOutputStream(fileOutputStream)){
+
+
+            byteArrayOutputStream.write(data);
+            byteArrayOutputStream.writeTo(fileOutputStream);
+
+            dataOutputStream.writeUTF(A);
+            dataOutputStream.writeUTF(B);
+
+
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+
+    }
 }
