b080032c5f7246336241d841b2f71f05aacef6a1
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-07-09 20:47:43 +0300

    Car

---
+++ v8/Controller/Car.java
@@ -0,0 +1,12 @@
+package com.MAX.v8.Controller;
+
+public class Car {
+    private String value;
+
+    public void setValue(String value){
+        this.value = value;
+    }
+    public String getValue(){
+        return value;
+    }
+}
