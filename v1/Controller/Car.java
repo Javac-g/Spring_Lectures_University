ebe8dc2e8a7db71787700cc50d83f562f36c4445
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2023-04-19 23:37:24 +0300

    Update Car.java

--- v1/Controller/Car.java
+++ v1/Controller/Car.java
@@ -3,8 +3,8 @@ package com.MAX.v1.Controller;
 public class Car {
     String value;
 
-    public String getValue() {
-        return value;
+    public void getValue() {
+      
     }
 
     public void setValue(String value) {
